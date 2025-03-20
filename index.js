ccb539d36f5cf692b1f4206c9e9094983b969b03
Anay Saha <anaysahapaban@gmail.com>
2025-03-21 03:54:06 +0600

    push

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 //  mongodb user and pass
@@ -48,7 +48,15 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/user/:id', async(req, res) =>{
+      const id = req.params.id;
+      console.log('please delete from database', id)
+      const query = { _id: new ObjectId id };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
 
+
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
